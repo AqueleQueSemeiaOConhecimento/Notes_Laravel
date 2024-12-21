28514a1ffcd686f014c75d6dfebbcfd46bd606ce
Matheus <D3rr3tido@gmail.com>
2024-12-21 12:49:05 -0300

    008

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,12 @@ class AuthController extends Controller
 {
     public function login()
     {
-        echo "login";
+        return view("login");
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        echo "loginSubmit";
     }
 
     public function logout()
