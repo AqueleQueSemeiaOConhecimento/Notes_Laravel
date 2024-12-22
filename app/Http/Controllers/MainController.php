8762b3a24aee6262c8a497d54eba072f87a73423
Matheus <D3rr3tido@gmail.com>
2024-12-22 17:46:35 -0300

    019

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    
+    public function index()
+    {
+        echo 'Im inside the app!';
+    }   
+
+    public function newNote()
+    {
+        echo 'Im am creating a new note!';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIfLogged;
 use Illuminate\Support\Facades\Route;
 
 // auth routes
 
 Route::get('/login', [AuthController::class,'login']);
 Route::post('/loginSubmit', [AuthController::class,'loginSubmit']);
-Route::get('/logout', [AuthController::class,'logout']);
+
+Route::middleware([CheckIfLogged::class])->group(function() {
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class,'logout']);
+});
