807fa3c42faf58b862814040f250bc7688d08977
Matheus <D3rr3tido@gmail.com>
2024-12-22 16:41:13 -0300

    015

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +12,23 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // create multiple users
+        DB::table("users")->insert([
+            [
+                'username' => 'user1@gmail.com',
+                'password' => bcrypt('abc123456'),
+                'created_at' => date('Y-m-d H:i:s')
+            ], 
+            [
+                'username' => 'user2@gmail.com',
+                'password' => bcrypt('abc123456'),
+                'created_at' => date('Y-m-d H:i:s')
+            ], 
+            [
+                'username' => 'user3@gmail.com',
+                'password' => bcrypt('abc123456'),
+                'created_at' => date('Y-m-d H:i:s')
+            ], 
+        ]);
     }
 }
