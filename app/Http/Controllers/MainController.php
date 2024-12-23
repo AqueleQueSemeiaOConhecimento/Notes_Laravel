79caf29fa37c6f46ac694f1f19b2b7edbc43de00
Matheus <D3rr3tido@gmail.com>
2024-12-23 15:14:25 -0300

    028

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -24,23 +24,24 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        try {
-            $id = Crypt::decrypt($id);
-        }   catch (DecryptException $e) {
-            return redirect()->route('home'); 
-        }
-
+        $id = $this->decryptId($id);
         echo "Im editing note with id => " . $id;
     }
 
     public function deleteNote($id)
     {
+        $id = $this->decryptId($id);
+        echo "im deleting note with id => " . $id;
+    }
+
+    private function decryptId($id)
+    {
+        // check if $id is encrypted
         try {
             $id = Crypt::decrypt($id);
         }   catch (DecryptException $e) {
             return redirect()->route('home'); 
         }
-
-        echo "im deleting note with id => " . $id;
+        return $id;
     }
 }
