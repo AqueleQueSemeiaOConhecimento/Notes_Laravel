2aae8c9f9040fb7d1e39dffeb14e024417a842cd
Matheus <D3rr3tido@gmail.com>
2024-12-23 14:15:29 -0300

    024

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        // load user's notes
+        $id = session('user.id'); 
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
 
-        // show home view
-        return view("home");
+        echo '<pre>';
+        print_r($user);
+        print_r($notes);
+
+    
     }   
 
     public function newNote()
