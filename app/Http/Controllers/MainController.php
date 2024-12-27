f114ac02c9ff0656438343f082bf2c4ae128cf65
Matheus <D3rr3tido@gmail.com>
2024-12-27 15:54:50 -0300

    030

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -26,7 +27,35 @@ class MainController extends Controller
 
     public function newNoteSubmit(Request $request)
     {
-        echo "Im creating a new note";
+        // validate request
+        $request->validate(
+            [
+                        'text_title' => 'required|min:3|max:200',
+                        'text_note' => 'required|min:3|max:3000',
+                    ], 
+            [
+                        'text_title.required' => 'O titulo é obrigatório',
+                        'text_title.min' => 'O titulo deve ter pelo menos :min caracteres',
+                        'text_title.max' => 'O titulo deve ter no máximo :max caracteres',
+
+                        'text_note.required' => 'A nota é obrigatória',
+                        'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                        'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+                    ]
+                );
+
+        // get user id
+        $id = session('user.id');
+
+        // create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 
     public function editNote($id)
